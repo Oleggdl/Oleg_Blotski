56351cf188df177bf36418864b795a88264d0b18
Oleg Blotski <oblotskiyj@gmail.com>
2022-04-09 21:12:17 +0300

    init project

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css'
+import {Component} from "react"
+
+class App extends Component {
+
+
+
+    render() {
+        return (
+            <>
+
+            </>
+        )
+    }
+}
+
+export default App
